d52eb7999f13a48c94258f89d7b915f4fe3c693a
quartzs2 <csh001231@gmail.com>
2025-04-26 13:10:58 +0900

    feat: 헤더 구현(#11)
    - 페이지로 이동할 수 있는 링크들이 있는 헤더 구현

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import { NAVIGATION_URL } from "@/constants/url";
+import { Link } from "react-router";
+
+const Header = () => {
+  return (
+    <header className="mx-auto flex max-w-7xl items-center justify-between p-4">
+      <div className="text-xl font-bold">포켓몬 도감</div>
+      <nav className="flex gap-8 font-normal">
+        {NAVIGATION_URL.map(({ name, url }) => (
+          <Link key={url} to={url}>
+            {name}
+          </Link>
+        ))}
+      </nav>
+    </header>
+  );
+};
+export default Header;
