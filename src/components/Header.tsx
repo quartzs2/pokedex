b5fbe21365823ee02825d1b94f405586146a920d
quartzs2 <csh001231@gmail.com>
2025-04-28 17:55:27 +0900

    chore: 헤더 수정(#24)
    - 로고 눌렀을 때 메인페이지로 이동하도록 수정
    - 메인, 상세정보 내비게이션 제거
    - 찜 목록 아이콘으로 수정
    - constants 폴더 제거(NAVIGATION_URL 사용x)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,30 @@
+import { IoMdHeartEmpty, IoIosSearch } from "react-icons/io";
 import { useNavigate, Link } from "react-router";
-import { NAVIGATION_URL } from "@constants/url";
-import { IoIosSearch } from "react-icons/io";
 
 const Header = () => {
   const navigate = useNavigate();
 
   return (
     <header className="mx-auto flex max-w-7xl items-center justify-between gap-6 p-6">
-      <div className="mr-auto text-xl font-bold">포켓몬 도감</div>
-      <div className="flex items-center gap-2">
-        <IoIosSearch size={20} />
-        <input
-          className="w-62 border-b-2 border-gray-400 focus:border-blue-400 focus:outline-none"
-          onChange={(e) => navigate(`/search?pokemon=${e.target.value}`)}
-          placeholder="검색어를 입력해주세요"
-          type="text"
-        />
-      </div>
-      <nav className="flex gap-8 font-normal">
-        {NAVIGATION_URL.map(({ name, url }) => (
-          <Link key={url} to={url}>
-            {name}
+      <Link to={"/"}>
+        <div className="mr-auto text-xl font-bold">포켓몬 도감</div>
+      </Link>
+      <div className="flex items-center gap-6">
+        <div className="flex items-center gap-2">
+          <IoIosSearch size={20} />
+          <input
+            className="w-62 border-b-2 border-gray-400 focus:border-blue-400 focus:outline-none"
+            onChange={(e) => navigate(`/search?pokemon=${e.target.value}`)}
+            placeholder="검색어를 입력해주세요"
+            type="text"
+          />
+        </div>
+        <nav className="flex gap-8 font-normal">
+          <Link to={"/favorites"}>
+            <IoMdHeartEmpty size={20} />
           </Link>
-        ))}
-      </nav>
+        </nav>
+      </div>
     </header>
   );
 };
